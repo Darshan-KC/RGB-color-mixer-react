58de3beecfaae5f3a26442e9474d2eaeccee8c97
Darshan KC <darshanbahadurchhetri@gmail.com>
2025-09-22 21:25:21 +0545

    feat: modify app js file

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,28 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+// import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+      <h1>Color Mixer</h1>
+
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <label htmlFor="red">Red</label>
+        <input type="range" name="" id="red" min={0} max={255} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      
+      <div>
+        <label htmlFor="green">Green</label>
+        <input type="range" name="" id="green" min={0} max={255} />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <div>
+        <label htmlFor="blue">Blue</label>
+        <input type="range" name="" id="blue" min={0} max={255} />
+      </div>
     </>
   )
 }
