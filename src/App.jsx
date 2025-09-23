4a729aa8b21c3a963aaf57f252f5a09e4d52ae2e
Darshan KC <darshanbahadurchhetri@gmail.com>
2025-09-23 20:07:38 +0545

    feat: bg change according to the value of color

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,27 @@ import viteLogo from '/vite.svg'
 // import './App.css'
 
 function App() {
+  let [red, setRed] = useState(0);
+  let [green, setGreen] = useState(0);
+  let [blue, setBlue] = useState(0);
 
   return (
     <>
       <h1>Color Mixer</h1>
-
+      <div style={{backgroundColor:'rgb('+red+','+green+', '+blue+')', width:'200px', height: '200px'}}></div>
       <div>
         <label htmlFor="red">Red</label>
-        <input type="range" name="" id="red" min={0} max={255} />
+        <input type="range" value={red} onChange={(event)=>setRed(event.target.value)} name="" id="red" min={0} max={255} />
       </div>
       
       <div>
         <label htmlFor="green">Green</label>
-        <input type="range" name="" id="green" min={0} max={255} />
+        <input type="range" value={green} onChange={(event) => setGreen(event.target.value)} name="" id="green" min={0} max={255} />
       </div>
 
       <div>
         <label htmlFor="blue">Blue</label>
-        <input type="range" name="" id="blue" min={0} max={255} />
+        <input type="range" value={blue} onChange={(event) => setBlue(event.target.value)} name="" id="blue" min={0} max={255} />
       </div>
     </>
   )
